28997856b835069ae710e8cb5dfdbf6d5e23cd5f
Sébastien Corne <sebastien@seebz.net>
2018-07-23 14:12:27 +0200

    only load parent & child styles by default

--- functions.php
+++ functions.php
@@ -26,13 +26,13 @@ function mychild_setup() {
 	/**
 	 * Suppression des metas `generator`
 	 */
-	remove_action( 'wp_head', 'wp_generator' );
+	// remove_action( 'wp_head', 'wp_generator' );
 
 	/**
 	 * Désactivation du support des Emoji
 	 */
-	remove_action( 'wp_head',         'print_emoji_detection_script', 7 );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	// remove_action( 'wp_head',         'print_emoji_detection_script', 7 );
+	// remove_action( 'wp_print_styles', 'print_emoji_styles' );
 }
 add_action( 'after_setup_theme', 'mychild_setup' );
 
@@ -50,7 +50,7 @@ function mychild_scripts() {
 	// Scripts
 	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	wp_enqueue_script( 'mychild-script', get_stylesheet_directory_uri() . '/assets/js/script' . $suffix . '.js', array( 'jquery' ), $mychild_version, true );
+	// wp_enqueue_script( 'mychild-script', get_stylesheet_directory_uri() . '/assets/js/script' . $suffix . '.js', array( 'jquery' ), $mychild_version, true );
 }
 add_action( 'wp_enqueue_scripts', 'mychild_scripts' );
 
